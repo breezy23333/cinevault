faf2d33642aa96393e86e39048de6ac4e6f3b8d3
breezy23333 <luvomaphela0@gmail.com>
2026-04-22 14:13:10 +0200

    force google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,16 @@ export default function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen bg-[#0b0f1a] text-white antialiased flex flex-col">
-        <main className="flex-1">{children}</main>
-      </body>
-    </html>
-  );
+  <html lang="en">
+    <head>
+      <meta
+        name="google-site-verification"
+        content="V4XKtKx6YSlNj3fUJoY4uI9bwoPpQIlKka_B-yZhqhE"
+      />
+    </head>
+    <body className="min-h-screen bg-[#0b0f1a] text-white antialiased flex flex-col">
+      <main className="flex-1">{children}</main>
+    </body>
+  </html>
+);
 }
