0c8134906d4afb14223b7eea6afc3f611f09680f
breezy23333 <luvomaphela0@gmail.com>
2026-04-24 17:54:07 +0200

    Add Google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "CineVault",
   description: "Discover movies and shows on CineVault.",
+  verification: {
+    google: "V4XKtKx6YSlNj3fUJoY4uI9bwoPpQIlKka_B-yZhqhE",
+  },
 };
 
 export default function RootLayout({
