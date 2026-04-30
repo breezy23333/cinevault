4822cf1830eca5406e821a955a1a67e121af7913
breezy23333 <luvomaphela0@gmail.com>
2026-04-30 16:19:24 +0200

    Update sitemap routes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,32 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://cinevault-tau-drab.vercel.app";
+
   return [
     {
-      url: "https://cinevault-tau-drab.vercel.app",
+      url: baseUrl,
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/top`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/trending`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/search`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
     },
   ];
 }
