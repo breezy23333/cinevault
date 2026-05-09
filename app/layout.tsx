685bf951c2447885fbde47b9a86fc3f76d419e91
breezy23333 <luvomaphela0@gmail.com>
2026-05-09 13:36:16 +0200

    Fix global footer layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-[#0b0f1a] text-white antialiased flex flex-col">
+      <body className="bg-[#0b0f1a] text-white antialiased overflow-x-hidden">
         <Navbar />
-        <main className="flex-1 pt-24">{children}</main>
-        <Footer />
+
+      <main className="pt-24">
+        {children}
+      </main>
+
+      <Footer />
       </body>
     </html>
   );
