e0f3aced5a00b733985e63eb772e9f2ca3a9af3f
breezy23333 <luvomaphela0@gmail.com>
2026-05-19 21:54:47 +0200

    Improve robots crawl rules

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,29 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://cinevault-tau-drab.vercel.app/sitemap.xml',
-  }
+    rules: [
+      {
+        userAgent: "*",
+        allow: [
+          "/",
+          "/movie/",
+          "/tv/",
+          "/anime",
+          "/cartoons",
+          "/top",
+          "/trending",
+        ],
+        disallow: [
+          "/api/",
+          "/admin/",
+          "/checkout/",
+          "/profile/",
+          "/rooms/",
+        ],
+      },
+    ],
+    sitemap: "https://cinevault-tau-drab.vercel.app/sitemap.xml",
+    host: "https://cinevault-tau-drab.vercel.app",
+  };
 }
