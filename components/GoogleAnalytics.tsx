42b11e205066749e619d75d42c9e515218923868
breezy23333 <luvomaphela0@gmail.com>
2026-08-30 17:48:49 +0200

    Delay analytics to improve initial page performance

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -9,16 +9,22 @@ export default function GoogleAnalytics() {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
 
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script
+        id="google-analytics"
+        strategy="lazyOnload"
+      >
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
 
-          gtag('config', '${GA_ID}');
+          function gtag() {
+            window.dataLayer.push(arguments);
+          }
+
+          gtag("js", new Date());
+          gtag("config", "${GA_ID}");
         `}
       </Script>
     </>
